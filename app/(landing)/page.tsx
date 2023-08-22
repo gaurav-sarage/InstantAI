dc1c3a1421d8d58e3d87fb9717d5d9b090762394
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-08-23 01:05:00 +0530

    added route for signin and signup page with buttons

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const LandingPage = () => {
     return (
         <div>
             Landing Page (Unprotected)
+            <div>
+                <Link href="/sign-in">
+                    <Button>
+                        Login
+                    </Button>
+                </Link>
+                <Link href="/sign-up">
+                    <Button>
+                        Register
+                    </Button>
+                </Link>                
+            </div>
         </div>
     );
 }
