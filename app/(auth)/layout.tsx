58ff01815081c8200c9c8beca3047bffb2f6312e
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-08-23 01:05:21 +0530

    code changed to auth

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+const AuthLayout = ({
+    children
+}: {
+    children: React.ReactNode;
+}) => {
+    return (
+        <div className="flex items-center justify-center h-full">
+            {children}
+        </div>
+    )
+}
+
+export default AuthLayout;
