e5a83ba85782a8151ebf9ba32a2fbecee3ec38a7
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-08-27 03:14:07 +0530

    created 2 variables to select the no of images and the resolution of images the user wants to generate

--- app/(dashboard)/(routes)/image/constants.ts
+++ app/(dashboard)/(routes)/image/constants.ts
@@ -7,3 +7,41 @@ export const formSchema = z.object({
     amount: z.string().min(1),
     resolution: z.string().min(1)
 });
+
+export const amountOptions = [
+    {
+        value: "1",
+        label: "1 Photo",
+    },
+    {
+        value: "2",
+        label: "2 Photos",
+    },
+    {
+        value: "3",
+        label: "3 Photos",
+    },
+    {
+        value: "4",
+        label: "4 Photos",
+    },
+    {
+        value: "5",
+        label: "5 Photos",
+    },    
+];
+
+export const resolutionOptions = [
+    {
+        value: "256x256",
+        label: "256x256",
+    },
+    {
+        value: "512x512",
+        label: "512x512",
+    },
+    {
+        value: "1024x1024",
+        label: "1024x1024",
+    },
+];
