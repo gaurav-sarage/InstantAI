1f4b61e8a1e0a652ba139859d0fca82c2b91a501
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-01 01:20:21 +0530

    added ignoredRoutes to handle the image generation issue of hostname and created folder for music generation

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/"]
+  publicRoutes: ["/", "/image"],
+  ignoredRoutes: ["/((?!api|trpc))(_next|.+\..+)(.*)", "/image"]
 })
 
 export const config = {
