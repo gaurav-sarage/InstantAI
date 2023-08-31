382ccb2c57c2862416c1b9a8a322fbb723b49fbb
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-01 01:15:15 +0530

    added the config for image to handle the hostname error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "oaidalleapiprodscus.blob.core.windows.net",
+        ]
+    }
+}
 
 module.exports = nextConfig
