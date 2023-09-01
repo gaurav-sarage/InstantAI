bc9a57ff70a9c6a570682e5455d2717a4af474dd
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-02 00:42:17 +0530

    finished building the route for music using replicate and created an response variable that takes the prompt from user

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -1,12 +1,10 @@
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
-import { Configuration, OpenAIApi } from "openai";
+import Replicate from "replicate";
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
-});
-
-const openai = new OpenAIApi(configuration);
+const replicate = new Replicate({
+    auth: process.env.REPLICATE_API_TOKEN!
+})
 
 export async function POST(
     req: Request
@@ -14,29 +12,29 @@ export async function POST(
     try {
         const { userId } = auth();
         const body = await req.json();
-        const { messages } = body;
+        const { prompt } = body;
 
         if(!userId) {
             return new NextResponse("Unauthorized", { status: 401 })
         }
 
-        if(!configuration.apiKey) {
-            return new NextResponse("OpenAI APi key not configured", { status: 500 })
+        if(!prompt) {
+            return new NextResponse("Prompt is required", { status: 400 })
         }
 
-        if(!messages) {
-            return new NextResponse("Messages are required", { status: 400 })
-        }
+        const response = await replicate.run(
+            "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+            {
+              input: {
+                prompt_a: prompt
+              }
+            }
+          );
 
-        const response = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages
-        });
-
-        return NextResponse.json(response.data.choices[0].message);
+        return NextResponse.json(response);
 
     } catch (error) {
-        console.log("[CONVERSTION_ERROR]", error);
+        console.log("[MUSIC_ERROR]", error);
         return new NextResponse("Internal error", { status: 500 })  
     }
 }
