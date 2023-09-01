f5d76300cd733f53a98fc6b04bf7c210eaea547e
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-02 00:53:46 +0530

    finished building the music page & added audio tag of native html to render the music generated from replicate ai

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -94,9 +94,11 @@ const MusicPage = () => {
                     {!music && !isLoading && (
                         <Empty label="No Music Generated" />
                     )}
-                    <div>
-                        Music will be generated here
-                    </div>
+                    {music && (
+                        <audio controls className="w-full mt-8">
+                            <source src={music} />
+                        </audio>
+                    )}
                 </div>
             </div>
         </div>
