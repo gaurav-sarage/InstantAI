8fb87edf6e431fb42e9407a80205bf26731f023c
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-05 22:40:30 +0530

    completed setting the route for video and added the api for video with prompt to be taken from user to generate the video

--- app/(dashboard)/(routes)/video/constants.ts
+++ app/(dashboard)/(routes)/video/constants.ts
@@ -2,6 +2,6 @@ import * as z from "zod";
 
 export const formSchema = z.object({
     prompt: z.string().min(1, {
-        message: "Prompt is required",
+        message: "Video prompt is required",
     }),
 });
---
+++ app/api/video/route.ts
@@ -0,0 +1,40 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import Replicate from "replicate";
+
+const replicate = new Replicate({
+    auth: process.env.REPLICATE_API_TOKEN!
+})
+
+export async function POST(
+    req: Request
+) {
+    try {
+        const { userId } = auth();
+        const body = await req.json();
+        const { prompt } = body;
+
+        if(!userId) {
+            return new NextResponse("Unauthorized", { status: 401 })
+        }
+
+        if(!prompt) {
+            return new NextResponse("Prompt is required", { status: 400 })
+        }
+
+        const response = await replicate.run(
+            "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
+            {
+              input: {
+                prompt
+              }
+            }
+          );
+
+        return NextResponse.json(response);
+
+    } catch (error) {
+        console.log("[VIDEO_ERROR]", error);
+        return new NextResponse("Internal error", { status: 500 })  
+    }
+}
