b75de15295a5a8a230d7e94052d67cb88b6214b9
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-15 15:57:34 +0530

    debug - changed the mode to dev from prod

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,3 +1,4 @@
+
 import { PrismaClient } from "@prisma/client";
 
 declare global {
@@ -9,3 +10,4 @@ const prismadb = globalThis.prisma || new PrismaClient();
 if(process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
 
 export default prismadb;
+
