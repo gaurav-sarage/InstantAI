319e8abb19034d529ef0d86def11e29e75162df2
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-15 23:11:08 +0530

    repeated the process for video route to check for freeApiLimit and increaseApiLimit to perform more actions

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import Replicate from "replicate";
 
+import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
+
 const replicate = new Replicate({
     auth: process.env.REPLICATE_API_TOKEN!
 })
@@ -22,6 +24,12 @@ export async function POST(
             return new NextResponse("Prompt is required", { status: 400 })
         }
 
+        const freeTrial = await checkApiLimit();
+
+        if(!freeTrial) {
+            return new NextResponse("Free Trial has expired", {status: 403})
+        }
+
         const response = await replicate.run(
             "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
             {
@@ -29,7 +37,9 @@ export async function POST(
                 prompt
               }
             }
-          );
+        );
+
+        await increaseApiLimit();
 
         return NextResponse.json(response);
 
@@ -37,4 +47,4 @@ export async function POST(
         console.log("[VIDEO_ERROR]", error);
         return new NextResponse("Internal error", { status: 500 })  
     }
-}
+};
