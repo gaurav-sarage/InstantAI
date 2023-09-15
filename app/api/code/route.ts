46e3d1ef182c920b4ba88fc3425f9440291b9c63
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-15 23:05:22 +0530

    repeated the freeApiLimit process as from conversation to code once user crosses the free tier

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import { Configuration, OpenAIApi, ChatCompletionRequestMessage } from "openai";
 
+import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
+
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
 });
@@ -33,10 +35,18 @@ export async function POST(
             return new NextResponse("Messages are required", { status: 400 })
         }
 
+        const freeTrial = await checkApiLimit();
+
+        if(!freeTrial) {
+            return new NextResponse("Free Trial has expired", {status: 403})
+        }
+
         const response = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
             messages: [instructionMessage, ...messages]
         });
+
+        await increaseApiLimit();
 
         return NextResponse.json(response.data.choices[0].message);
 
