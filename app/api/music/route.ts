0222894be298997040718fb091b49a4bcedac082
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-15 23:08:37 +0530

    repeated the process for music route

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import Replicate from "replicate";
 
+import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
+
 const replicate = new Replicate({
     auth: process.env.REPLICATE_API_TOKEN!
 })
@@ -22,6 +24,12 @@ export async function POST(
             return new NextResponse("Prompt is required", { status: 400 })
         }
 
+        const freeTrial = await checkApiLimit();
+
+        if(!freeTrial) {
+            return new NextResponse("Free Trial has expired", {status: 403})
+        }
+
         const response = await replicate.run(
             "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
             {
@@ -29,7 +37,9 @@ export async function POST(
                 prompt_a: prompt
               }
             }
-          );
+        );
+
+        await increaseApiLimit();
 
         return NextResponse.json(response);
 
