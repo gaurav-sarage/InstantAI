b8d9ce2fba5a1cb104aac5f102a80e27b0f47098
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-16 18:32:39 +0530

    imported getApiLimitCount and created a variable to display on frontend

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,16 @@
 import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
+import { getApiLimitCount } from "@/lib/api-limit";
 
-const DashboardLayout = ({
+const DashboardLayout = async ({
     children
 }: {
     children: React.ReactNode;
 }) => {
+
+    const apiLimitCount = await getApiLimitCount();
+
+
     return (
         <div className="h-full relative">
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
