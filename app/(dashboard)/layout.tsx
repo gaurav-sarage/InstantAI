92ac57f9d4288bf3aaa99f68eca48ec722cf6ab9
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-16 22:57:19 +0530

    changed the layout for apiLimitCount inside the dashboard

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -10,11 +10,10 @@ const DashboardLayout = async ({
 
     const apiLimitCount = await getApiLimitCount();
 
-
     return (
         <div className="h-full relative">
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-                <Sidebar />
+                <Sidebar apiLimitCount={apiLimitCount}/>
             </div>
             <main className="md:pl-72">
                 <Navbar />
