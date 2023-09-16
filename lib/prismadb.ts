8a3fc685297b40ab159c714d8eb3ad3e5bde0b32
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-16 22:06:45 +0530

    updated code

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client"
 
-declare global {
-    var prisma: PrismaClient | undefined;
-};
+// declare global {
+//     var prisma: PrismaClient | undefined;
+// };
 
-const prismadb = globalThis.prisma || new PrismaClient();
+// const prismadb = globalThis.prisma || new PrismaClient()
 
-if(process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+// if(process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
 
-export default prismadb;
+// export default prismadb;
