b2e840e6892b231844addeb6df4ed28e860f9296
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-17 05:24:19 +0530

    finished building the freeLimitCounter and created a button to upgrade the free plan. imported progress from shadcn to mark the progress with each api action

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -3,6 +3,9 @@
 import { useEffect, useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { MAX_FREE_COUNTS } from "@/constants";
+import { Progress } from "@/components/ui/progress";
+import { Button } from "@/components/ui/button";
+import { Zap } from "lucide-react";
 
 interface FreeCounterProps {
     apiLimitCount: number;
@@ -30,7 +33,15 @@ export const FreeCounter = ({
                         <p>
                             {apiLimitCount} / {MAX_FREE_COUNTS} Free Generations
                         </p>
+                        <Progress
+                            className="h-3"
+                            value={(apiLimitCount / MAX_FREE_COUNTS) * 100}
+                        />
                     </div>
+                    <Button className="w-full" variant="premium">
+                        Upgrade
+                        <Zap className="w-4 h-4 ml-2 fill-white"/>
+                    </Button>
                 </CardContent>
             </Card>
         </div>
