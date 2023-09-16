4b2d0ef730c3171011fe132d8a200d5faceaba89
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-16 22:18:56 +0530

    feature - created a component to display no. of times the actions have been performed over no. of max free actions

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -1,7 +1,38 @@
-export const FreeCounter = () => {
+"use client";
+
+import { useEffect, useState } from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import { MAX_FREE_COUNTS } from "@/constants";
+
+interface FreeCounterProps {
+    apiLimitCount: number;
+};
+
+
+export const FreeCounter = ({
+    apiLimitCount = 0
+}: FreeCounterProps) => {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true)
+    }, []);
+
+    if(!mounted) {
+        return null;
+    }
+
     return (
-        <div>
-            FreeCounter
+        <div className="px-3">
+            <Card className="bg-white/10 border-0">
+                <CardContent className="py-6">
+                    <div className="text-center text-sm text-white mb-4 space-y-2">
+                        <p>
+                            {apiLimitCount} / {MAX_FREE_COUNTS} Free Generations
+                        </p>
+                    </div>
+                </CardContent>
+            </Card>
         </div>
     )
 }
