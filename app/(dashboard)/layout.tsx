0b110c394e096adc392158e68746b747314e764b
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-17 17:21:01 +0530

    changed the (upgrade to pro) button layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -12,7 +12,7 @@ const DashboardLayout = async ({
 
     return (
         <div className="h-full relative">
-            <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
+            <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 bg-gray-900">
                 <Sidebar apiLimitCount={apiLimitCount}/>
             </div>
             <main className="md:pl-72">
