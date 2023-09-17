2278d55f9baf65b91545089fd43546125fa161a3
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-18 03:45:28 +0530

    repeated the same process for music generation in the (dashboard) once the user hits the free usage limit

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -15,9 +15,11 @@ import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import { Empty } from "@/components/empty";
 import { Loader } from "@/components/loader";
+import { useProModal } from "@/hooks/use-pro-modal";
 
 
 const MusicPage = () => {
+    const proModal = useProModal();
     const router = useRouter();
     const [music, setMusic] = useState<string>();
 
@@ -41,7 +43,9 @@ const MusicPage = () => {
             form.reset();
 
         } catch(error: any) {
-            console.log(error)
+            if(error?.response?.status === 403) {
+                proModal.onOpen();
+            }
         } finally {
             router.refresh()
         }
