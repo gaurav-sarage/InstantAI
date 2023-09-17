4add2554a040e4a9366f73e87ee468b657e273be
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-17 17:20:32 +0530

    created a button to upgrade to pro and imported the variant

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,9 +1,28 @@
 "use client";
 
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { useProModal } from "@/hooks/use-pro-modal";
+import { Badge } from "@/components/ui/badge";
+
 export const ProModal = () => {
+
+    const proModal = useProModal();
+
+    
     return (
-        <div>
-            Pro Modal
-        </div>
+        <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
+                        <div className="flex items-center gap-x-2 font-bold py-1">
+                            Upgrade to InstantAI
+                            <Badge variant="premium" className="uppercase text-sm py-1">
+                                pro
+                            </Badge>
+                        </div>
+                    </DialogTitle>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
     )
 };
