c38cb83c5c6d416e45906b0de68157180e4a0192
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-18 03:52:30 +0530

    added apiLimitCount and getApiLimit as props in the navbar to fix the count of free tier usage

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,14 @@
 import { UserButton } from "@clerk/nextjs";
 
 import MobileSidebar from "./mobile-sidebar";
+import { getApiLimitCount } from "@/lib/api-limit";
 
-const Navbar = () => {
+const Navbar = async () => {
+    const apiLimitCount = await getApiLimitCount();
+
     return (
         <div className="flex items-center p-4">
-            <MobileSidebar />
+            <MobileSidebar apiLimitCount={apiLimitCount} />
             <div className="flex w-full justify-end">
                 <UserButton afterSignOutUrl="/"/>
             </div>
