8541bfc01d555b49d31f45e8a36b0a18f29cc71c
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-18 03:29:37 +0530

    imported useProModal to use the onOpen method

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -5,7 +5,8 @@ import { Card, CardContent } from "@/components/ui/card";
 import { MAX_FREE_COUNTS } from "@/constants";
 import { Progress } from "@/components/ui/progress";
 import { Button } from "@/components/ui/button";
-import { Zap } from "lucide-react";
+import { Zap } from "lucide-react"
+import { useProModal } from "@/hooks/use-pro-modal";
 
 interface FreeCounterProps {
     apiLimitCount: number;
@@ -15,6 +16,7 @@ interface FreeCounterProps {
 export const FreeCounter = ({
     apiLimitCount = 0
 }: FreeCounterProps) => {
+    const proModal = useProModal();
     const [mounted, setMounted] = useState(false);
 
     useEffect(() => {
@@ -38,7 +40,7 @@ export const FreeCounter = ({
                             value={(apiLimitCount / MAX_FREE_COUNTS) * 100}
                         />
                     </div>
-                    <Button className="w-full" variant="premium">
+                    <Button onClick={proModal.onOpen} className="w-full" variant="premium">
                         Upgrade
                         <Zap className="w-4 h-4 ml-2 fill-white"/>
                     </Button>
