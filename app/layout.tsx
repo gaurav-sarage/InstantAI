1af3b3c2ea15ccae2585d3ed447e9c3dd175bf9d
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-17 05:50:34 +0530

    deleted the z-index from the sidebar, wasn't looking good :(

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import { ModalProvider } from '@/components/modal-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
