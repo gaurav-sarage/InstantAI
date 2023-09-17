746b6702539de942d48a661ebf9831247bf2bac8
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-18 03:28:56 +0530

    updated the correct package.json file and added an onClick fn to button whenever the user wants to upgrade to pro model

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,13 +1,66 @@
 "use client";
 
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle 
+} from "@/components/ui/dialog";
+
+import {
+    Check,
+    MessageSquare,
+    Music,
+    ImageIcon,
+    VideoIcon,
+    Code,
+    Zap
+} from "lucide-react";
+
 import { useProModal } from "@/hooks/use-pro-modal";
 import { Badge } from "@/components/ui/badge";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button";
+
+const tools = [
+    {
+      label: "Conversation",
+      icon: MessageSquare,
+      color: "text-violet-500",
+      bgColor: "bg-violet-500/10",
+    },
+    {
+      label: "Image Generation",
+      icon: ImageIcon,
+      color: "text-pink-700",
+      bgColor: "bg-pink-700/10",
+    },
+    {
+      label: "Video Generation",
+      icon: VideoIcon,
+      color: "text-orange-700",
+      bgColor: "bg-orange-700/10",
+    },
+    {
+      label: "Music Generation",
+      icon: Music,
+      color: "text-emerald-500",
+      bgColor: "bg-emerald-500/10",
+    },  
+    {
+      label: "Code Generation",
+      icon: Code,
+      color: "text-green-700",
+      bgColor: "bg-green-700/10",
+    }
+  ]
 
 export const ProModal = () => {
 
     const proModal = useProModal();
-
     
     return (
         <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -21,7 +74,35 @@ export const ProModal = () => {
                             </Badge>
                         </div>
                     </DialogTitle>
+                    <DialogDescription className="text-center pt-2 space-y-2 text-zinc-900 font-medium">
+                        {tools.map((tool) => (
+                            <Card
+                                key={tool.label}
+                                className="p-3 border-black/5 flex items-center justify-between"
+                            >
+                                <div className="flex items-center gap-x-4">
+                                    <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                                        <tool.icon className={cn("w-6 h-6", tool.color)} />
+                                    </div>
+                                    <div className="font-semibold text-sm">
+                                        {tool.label}
+                                    </div>
+                                </div>
+                                <Check className="text-primary w-5 h-5" />
+                            </Card>
+                        ))}
+                    </DialogDescription>
                 </DialogHeader>
+                <DialogFooter>
+                    <Button
+                        size="lg"
+                        variant="premium"
+                        className="w-full"
+                    >
+                        Upgrade
+                        <Zap className="w-4 h-4 ml-2 fill-white" />
+                    </Button>
+                </DialogFooter>
             </DialogContent>
         </Dialog>
     )
