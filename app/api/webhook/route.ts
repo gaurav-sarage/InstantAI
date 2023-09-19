7b3390b657867aa5f15862551c9e231c9f8ef713
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-20 05:06:05 +0530

    added 2nd event to check if the user upgraded the subscription after it expired/renew it

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -46,4 +46,27 @@ export async function POST(req: Request) {
             },
         });
     }
+
+    if(event.type === "invoice.payment_succeeded") {
+        const subscription = await stripe.subscriptions.retrieve(
+            session.subscription as string
+        );
+
+        await prismadb.userSubscription.update({
+            where: {
+                stripeSubscriptionId: subscription.id,
+            },
+            data: {
+                stripePriceId: subscription.items.data[0].price.id,
+                stripeCurrentPeriodEnd: new Date(
+                    subscription.current_period_end * 100,
+                ),
+            },
+        });
+    }
+
+    return new NextResponse(null, {
+        status: 400
+    });
+
 }
