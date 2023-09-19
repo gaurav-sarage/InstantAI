adbdea80273a09dfaa0ecaea5ec32d5e65e2c046
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-19 23:43:14 +0530

    created function to check for userSubscriptions if taken already, else create a new form to enter their details

---
+++ app/api/stripe/route.ts
@@ -0,0 +1,53 @@
+import { auth, currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+import { stripe } from "@/lib/stripe";
+import { absoluteUrl } from "@/lib/utils";
+
+const settingsUrl = absoluteUrl("/settings");
+
+export async function GET() {
+    try {
+        const { userId } = auth();
+        const user = await currentUser();
+
+        if(!userId || !user) {
+            return new NextResponse("Unauthorized", {
+                status: 401
+            });
+        }
+
+        const userSubscription = await prismadb.userSubscription.findUnique({
+            where: {
+                userId
+            }
+        });
+
+        if(userSubscription && userSubscription.stripeCustomerId) {
+            const stripeSession = await stripe.billingPortal.sessions.create({
+                customer: userSubscription.stripeCustomerId,
+                return_url: settingsUrl,
+            });
+
+            return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+        }
+
+        const stripeSession = await stripe.checkout.sessions.create({
+            success_url: settingsUrl,
+            cancel_url: settingsUrl,
+            payment_method_types: ["card"],
+            mode: "subscription",
+            billing_address_collection: "auto",
+            customer_email: user.emailAddresses[0].emailAddress,
+            
+        })
+
+    }
+    catch (error) {
+        console.log("[STRIPE_ERROR]", error);
+        return new NextResponse("Internal Error", {
+            status: 500
+        })        
+    }
+}
