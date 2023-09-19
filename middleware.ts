01cf1f196a7751ae6b31bf7f950583533b15e54b
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-20 04:39:41 +0530

    updated middleware file

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/image"],
-  ignoredRoutes: ["/((?!api|trpc))(_next|.+\..+)(.*)"]
+  publicRoutes: ["/"]
 })
 
 export const config = {
