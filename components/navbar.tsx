5a6caa8e95721951350cd56ee966b26bad404abd
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-20 16:23:11 +0530

    repeat - imported isPro to disable the free generations count from navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,13 +2,15 @@ import { UserButton } from "@clerk/nextjs";
 
 import MobileSidebar from "./mobile-sidebar";
 import { getApiLimitCount } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
 
 const Navbar = async () => {
     const apiLimitCount = await getApiLimitCount();
+    const isPro = await checkSubscription();
 
     return (
         <div className="flex items-center p-4">
-            <MobileSidebar apiLimitCount={apiLimitCount} />
+            <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} />
             <div className="flex w-full justify-end">
                 <UserButton afterSignOutUrl="/"/>
             </div>
