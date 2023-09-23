1792e34df4f00ed5bf1da2a0b7f83f96c7b37486
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-23 21:21:08 +0530

    repeat- imported checkSubscription lib and created isPro variable to check whether the user is currently on pro model and used await to increase the api limit

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 import Replicate from "replicate";
 
 import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
 
 const replicate = new Replicate({
     auth: process.env.REPLICATE_API_TOKEN!
@@ -25,6 +26,7 @@ export async function POST(
         }
 
         const freeTrial = await checkApiLimit();
+        const isPro = await checkSubscription();
 
         if(!freeTrial) {
             return new NextResponse("Free Trial has expired", {status: 403})
@@ -39,7 +41,9 @@ export async function POST(
             }
         );
 
-        await increaseApiLimit();
+        if(!isPro) {
+            await increaseApiLimit();
+        }
 
         return NextResponse.json(response);
 
