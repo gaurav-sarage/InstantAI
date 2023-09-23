958834c02d6fdadbb1ec4e70980d02ec25d0b802
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-09-23 21:09:54 +0530

    imported isPro set a conditional to check if the user is currently on pro model or not in the code directory of the application

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 import { Configuration, OpenAIApi, ChatCompletionRequestMessage } from "openai";
 
 import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
@@ -36,8 +37,9 @@ export async function POST(
         }
 
         const freeTrial = await checkApiLimit();
+        const isPro = await checkSubscription();
 
-        if(!freeTrial) {
+        if(!freeTrial && !isPro) {
             return new NextResponse("Free Trial has expired", {status: 403})
         }
 
@@ -46,7 +48,9 @@ export async function POST(
             messages: [instructionMessage, ...messages]
         });
 
-        await increaseApiLimit();
+        if(!isPro) {
+            await increaseApiLimit();
+        }
 
         return NextResponse.json(response.data.choices[0].message);
 
