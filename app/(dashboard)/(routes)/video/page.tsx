ec4c68ca17c3017660278d9ef794dd5de4e5c7c9
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-10-03 04:19:35 +0530

    installed react hot toast and created condition to display error msg if there is an error while generating output

--- app/(dashboard)/(routes)/video/page.tsx
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -16,6 +16,7 @@ import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import { Empty } from "@/components/empty";
 import { Loader } from "@/components/loader";
+import { toast } from "react-hot-toast";
 
 
 const VideoPage = () => {
@@ -45,6 +46,8 @@ const VideoPage = () => {
         } catch(error: any) {
             if(error?.response?.status === 403) {
                 proModal.onOpen();
+            } else {
+                toast.error("Something went wrong :(");
             }
         } finally {
             router.refresh()
