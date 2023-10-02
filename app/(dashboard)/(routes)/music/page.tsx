d6b6b9b3e60870012a4d358a5480b0c69af9ea66
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-10-03 04:19:12 +0530

    installed react hot toast and created condition to display error msg if there is an error while generating output

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -16,6 +16,7 @@ import { useRouter } from "next/navigation";
 import { Empty } from "@/components/empty";
 import { Loader } from "@/components/loader";
 import { useProModal } from "@/hooks/use-pro-modal";
+import { toast } from "react-hot-toast";
 
 
 const MusicPage = () => {
@@ -45,6 +46,8 @@ const MusicPage = () => {
         } catch(error: any) {
             if(error?.response?.status === 403) {
                 proModal.onOpen();
+            } else {
+                toast.error("Something went wrong :(");
             }
         } finally {
             router.refresh()
