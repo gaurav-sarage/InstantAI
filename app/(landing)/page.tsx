7d3afb1b0924e3272041e6ab561793113b0b94fa
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-10-09 22:54:29 +0530

    updated landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,22 +1,13 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { LandingContent } from "@/components/landing-content";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingNavbar } from "@/components/landing-navbar";
 
 const LandingPage = () => {
     return (
-        <div>
-            Landing Page (Unprotected)
-            <div>
-                <Link href="/sign-in">
-                    <Button>
-                        Login
-                    </Button>
-                </Link>
-                <Link href="/sign-up">
-                    <Button>
-                        Register
-                    </Button>
-                </Link>                
-            </div>
+        <div className="h-full">
+            <LandingNavbar />
+            <LandingHero />
+            <LandingContent />
         </div>
     );
 }
