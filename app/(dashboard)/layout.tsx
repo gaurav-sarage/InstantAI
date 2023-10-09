743351af744a4e5624e17cab38d9cd0192203b97
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-10-09 23:17:40 +0530

    fixed imports error

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,15 +1,14 @@
-import Navbar from "@/components/navbar";
-import Sidebar from "@/components/sidebar";
-import { getApiLimitCount } from "@/lib/api-limit";
-import { checkSubscription } from "@/lib/subscription";
+import Navbar from "@/components/navbar"
+import Sidebar from "@/components/sidebar"
+import { getApiLimitCount } from "@/lib/api-limit"
+import { checkSubscription } from "@/lib/subscription"
+
 
 const DashboardLayout = async ({
     children
 }: {
-    children: React.ReactNode;
+    children: React.ReactNode
 }) => {
-    
-    // Get API limit count for the current user and set it in localStorage to be used later on by other components
 
     const apiLimitCount = await getApiLimitCount();
     const isPro = await checkSubscription();
@@ -17,14 +16,14 @@ const DashboardLayout = async ({
     return (
         <div className="h-full relative">
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 bg-gray-900">
-                <Sidebar isPro={isPro} apiLimitCount={apiLimitCount}/>
+                <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
             </div>
             <main className="md:pl-72">
                 <Navbar />
                 {children}
             </main>
         </div>
-    );
+    )
 }
 
-export default DashboardLayout;
+export default DashboardLayout
