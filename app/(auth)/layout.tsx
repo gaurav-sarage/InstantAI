d68820e72c44289c5c100552f533e01022dc9d9b
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-10-09 22:54:11 +0530

    added layout in auth page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,6 @@
 const AuthLayout = ({
     children
-}: {
-    children: React.ReactNode;
-}) => {
+}: { children: React.ReactNode }) => {
     return (
         <div className="flex items-center justify-center h-full">
             {children}
