998dfa906f31876a9e757f9314b638a8553e6bfe
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-10-10 16:01:42 +0530

    updated the dashboard page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,93 +1,81 @@
-"use client";
+"use client"
 
-import {
-  MessageSquare,
-  ArrowRight,
-  Music,
-  ImageIcon,
-  VideoIcon,
-  Code
-} from "lucide-react";
 import { useRouter } from "next/navigation";
 
-
-import { cn } from "@/lib/utils";
 import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { MessageSquare, ArrowRight, Music, ImageIcon, VideoIcon,Code } from "lucide-react"
 
 const tools = [
-  {
-    label: "Conversation",
-    icon: MessageSquare,
-    color: "text-violet-500",
-    bgColor: "bg-violet-500/10",
-    href: "/conversation"
-  },
-  {
-    label: "Image Generation",
-    icon: ImageIcon,
-    color: "text-pink-700",
-    bgColor: "bg-pink-700/10",
-    href: "/image"
-  },
-  {
-    label: "Video Generation",
-    icon: VideoIcon,
-    color: "text-orange-700",
-    bgColor: "bg-orange-700/10",
-    href: "/video"
-  },
-  {
-    label: "Music Generation",
-    icon: Music,
-    color: "text-emerald-500",
-    bgColor: "bg-emerald-500/10",
-    href: "/music"
-  },  
-  {
-    label: "Code Generation",
-    icon: Code,
-    color: "text-green-700",
-    bgColor: "bg-green-700/10",
-    href: "/code"
-  }
-]
+    {
+        label: "Conversation",
+        icon: MessageSquare,
+        color: "text-violet-500",
+        bgColor: "bg-violet-500/10",
+        href: "/conversation"
+    },
+    {
+        label: "Music Generation",
+        icon: Music,
+        color: "text-emerald-500",
+        bgColor: "bg-emerald-500/10",
+        href: "/music"
+    },
+    {
+        label: "Image Generation",
+        icon: ImageIcon,
+        color: "text-pink-700",
+        bgColor: "bg-pink-700/10",
+        href: "/image"
+    },
+    {
+        label: "Video Generation",
+        icon: VideoIcon,
+        color: "text-orange-700",
+        bgColor: "bg-orange-700/10",
+        href: "/video"
+    },
+    {
+        label: "Code Generation",
+        icon: Code,
+        color: "text-green-700",
+        bgColor: "bg-green-700/10",
+        href: "/code"
+    }
+];
 
 const DashboardPage = () => {
-
-  const router = useRouter();
-
-
-  return (
-    <div>
-      <div className="mb-8 space-y-4">
-        <h2 className="text-2xl md:text-4xl font-bold text-center">
-          Explore the power of AI
-        </h2>
-        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-          Chat with the smartest AI - Experience the power of AI
-        </p>
-      </div>
-      <div className="px-4 md:px-20 lg:px-32 space-y-4">
-        {tools.map((tool) => (
-          <Card
-            onClick={() => router.push(tool.href)}
-            key={tool.href}
-            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
-          >
-            <div className="flex items-center gap-x-4">
-              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
-                <tool.icon className={cn("w-8 h-8", tool.color)} />
-              </div>
-              <div className="font-semibold">
-                {tool.label}
-              </div>
+    const router = useRouter()
+    return (
+        <div>
+            <div className="mb-8 space-y-4">
+                <h2 className="text-2xl md:text-4xl font-bold text-center">
+                Explore the power of AI
+                </h2>
+                <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+                Chat with the smartest AI - Experience the power of AI
+                </p>
             </div>
-            <ArrowRight className="w-5 h-5" />
-          </Card>
-        ))}
-      </div>
-    </div>
-  )
+            <div className="px-4 md:px-20 lg:px-32 space-y-4">
+            {tools.map((tool) => (
+                <Card 
+                onClick={()=>router.push(tool.href)}
+                key={tool.href}
+                className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+                >
+                    <div className="flex items-center gap-x-4">
+                        <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                            <tool.icon className={cn("w-8 h-8",tool.color)}/>
+                        </div>
+                        <div className="font-semibold">
+                            {tool.label}
+                        </div>
+                    </div>
+                    <ArrowRight className="w-5 h-5" />
+                </Card>
+            ))}
+            </div>
+        </div>
+    );
 }
-
 export default DashboardPage;
