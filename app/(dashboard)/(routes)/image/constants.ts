8d50a595a90a9662bb68e3851324df616aed4ba5
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-10-10 16:01:16 +0530

    updated constants for image file

--- app/(dashboard)/(routes)/image/constants.ts
+++ app/(dashboard)/(routes)/image/constants.ts
@@ -2,33 +2,33 @@ import * as z from "zod";
 
 export const formSchema = z.object({
     prompt: z.string().min(1, {
-        message: "Image Prompt is required",
+        message: "Photo prompt is required"
     }),
     amount: z.string().min(1),
-    resolution: z.string().min(1)
+    resolution: z.string().min(1),
 });
 
 export const amountOptions = [
     {
         value: "1",
-        label: "1 Photo",
+        label: "1 Photo"
     },
     {
         value: "2",
-        label: "2 Photos",
+        label: "2 Photos"
     },
     {
         value: "3",
-        label: "3 Photos",
+        label: "3 Photos"
     },
     {
         value: "4",
-        label: "4 Photos",
+        label: "4 Photos"
     },
     {
         value: "5",
-        label: "5 Photos",
-    },    
+        label: "5 Photos"
+    }
 ];
 
 export const resolutionOptions = [
