5b9e44f933ea7ee1dc6b03852b19782558c9c3ff
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-10-16 23:09:54 +0530

    completed music route page

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -1,115 +1,115 @@
 "use client";
 
-import axios from "axios";
 import * as z from "zod";
-import { Music } from "lucide-react";
+import axios from "axios";
+import { MessageSquare, Music } from "lucide-react";
 import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { ChatCompletionRequestMessage } from "openai";
 
 import { Heading } from "@/components/heading";
-import { formSchema } from "./constants";
-import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+
+import { formSchema } from "./constants";
 import { Empty } from "@/components/empty";
 import { Loader } from "@/components/loader";
 import { useProModal } from "@/hooks/use-pro-modal";
-import { toast } from "react-hot-toast";
-
+import toast from "react-hot-toast";
 
 const MusicPage = () => {
-    const proModal = useProModal();
-    const router = useRouter();
-    const [music, setMusic] = useState<string>();
+  const proModal = useProModal();
+  const router = useRouter();
+  const [music, setMusic] = useState<string>();
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            prompt: ""
-        }
-    });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
 
-    const isLoading = form.formState.isSubmitting;
+  const isLoading = form.formState.isSubmitting;
 
-    const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        try {
-            setMusic(undefined);
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setMusic(undefined);
 
-            const response = await axios.post("/api/music", values);
+      const response = await axios.post("/api/music", values);
 
-            setMusic(response.data.audio);
+      setMusic(response.data.audio);
+      toast.success("You did it!")
+      form.reset();
+    } catch (error: any) {
+      if (error?.response?.status === 403) {
+        proModal.onOpen();
+      } else {
+        toast.error("Replicate Free Subscription Limit Expired :(")
+      }
+    } finally {
+      router.refresh();
+    }
+  };
 
-            form.reset();
-
-        } catch(error: any) {
-            if(error?.response?.status === 403) {
-                proModal.onOpen();
-            } else {
-                toast.error("Something went wrong :(");
-            }
-        } finally {
-            router.refresh()
-        }
-    };
-
-
-    return (
+  return (
+    <div>
+      <Heading
+        title="Music Generation"
+        description="Turn your prompt into music."
+        icon={Music}
+        iconColor="text-emerald-500"
+        bgColor="bg-emerald-500/10"
+      />
+      <div className="px-4 lg:px-8">
         <div>
-            <Heading 
-                title="Music Generation"
-                description="Turn your prompt into music"
-                icon={Music}
-                iconColor="text-emerald-500"
-                bgColor="bg-emerald-500/10"
-            />
-            <div className="px-4 lg:px-8">
-                <div>
-                    <Form {...form}>
-                        <form
-                            onSubmit={form.handleSubmit(onSubmit)}
-                            className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
-                        >
-                            <FormField 
-                                name="prompt"
-                                render={({ field }) => (
-                                    <FormItem className="col-span-12 lg:col-span-10">
-                                        <FormControl className="m-0 p-0">
-                                            <Input
-                                                className="border-0 outline-none focus-visible:ring-0 focus-visible: ring-transparent"
-                                                disabled={isLoading}
-                                                placeholder="Piano Solo"
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                    </FormItem>
-                                )}
-                            />
-                            <Button className="col-span-12 lg:col-span-2 w-full" disabled={isLoading}>
-                                Generate
-                            </Button>
-                        </form>
-                    </Form>
-                </div>
-                <div className="space-y-4 mt-4">
-                    {isLoading && (
-                        <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
-                            <Loader />
-                        </div>
-                    )}
-                    {!music && !isLoading && (
-                        <Empty label="No Music Generated" />
-                    )}
-                    {music && (
-                        <audio controls className="w-full mt-8">
-                            <source src={music} />
-                        </audio>
-                    )}
-                </div>
-            </div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="rounded-lg border w-full px-3 p-4 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="Piano solo"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                disabled={isLoading}
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
         </div>
-    );
-}
 
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+              <Loader />
+            </div>
+          )}
+          {!music && !isLoading && <Empty label="No Music Generated." />}
+          {music && (
+            <audio controls className="w-full mt-8">
+              <source src={music} />
+            </audio>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 export default MusicPage;
