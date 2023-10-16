dae5fdc41f690eeee4541291d5514379960bbf1a
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-10-16 23:13:18 +0530

    completed building page layout for settings route, added option for user to upgrade their plan

--- app/(dashboard)/(routes)/settings/page.tsx
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -1,33 +1,28 @@
-import { Settings } from "lucide-react";
 import { Heading } from "@/components/heading";
-import { checkSubscription } from "@/lib/subscription";
 import { SubscriptionButton } from "@/components/subscription-button";
+import { checkSubscription } from "@/lib/subscription";
+import { Settings } from "lucide-react";
 
-const SettingsPage = async () => {
+const SettingPage = async () => {
 
-    const isPro = await checkSubscription();
-
+    const isPro = await checkSubscription()
     return (
         <div>
             <Heading
                 title="Settings"
-                description="Manage account settings"
+                description="Manage account settings."
                 icon={Settings}
-                iconColor="text-gray-700"
-                bgColor="text-gray-700/10"
+                iconColor="text-grey-700"
+                bgColor="bg-grey-700/10"
             />
             <div className="px-4 lg:px-8 space-y-4">
                 <div className="text-muted-foreground text-sm">
-                    {
-                        isPro ? 
-                        "You are currently on a pro plan" : 
-                        "You are currently on a free plan"
-                    }
+                    {isPro ? "You are currently on a pro plan." : "You are currently on a free plan"}
                 </div>
                 <SubscriptionButton isPro={isPro} />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default SettingsPage;
+export default SettingPage;
