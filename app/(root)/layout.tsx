f03295471613c6d71573d73ddc9caf15f847b555
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-08 02:42:39 +0530

    Imported Sidebar & MobileNav component in the `page.tsx` file

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import MobileNav from '@/components/global/MobileNav';
+import Sidebar from '@/components/global/Sidebar';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className='root'>
+      <Sidebar />
+      <MobileNav />
+
       <div className='root-container'>
         <div className='wrapper'>
             {children}
