6442b520b5b294d52870462f0265d09d78357d7e
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-07 19:45:01 +0530

    Initialized NextJS App

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const IBMPlex = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex"
+});
+
+export const metadata: Metadata = {
+  title: "InstantAI",
+  description: "AI powered Image Generator",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+      </body>
+    </html>
+  );
+}
