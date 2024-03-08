888197d59e4ba95b227a6254fcecb5fb290d603e
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-09 03:18:18 +0530

    Added header component
    Implemented a reusable React component for displaying titles & subtitles (optional) with conditional rendering

---
+++ components/global/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = ({ title, subtitle }: { title: string, subtitle?: string }) => {
+  return (
+    <>
+        <h2 className='h2-bold text-black'>
+            {title}
+        </h2>    
+        {subtitle && <p className='p-16-regular mt-4'>
+            {subtitle}
+        </p>}
+    </>
+  )
+}
+
+export default Header
