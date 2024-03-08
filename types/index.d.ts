988919c4ad27eb0bad542d160fae0e3ec2494c55
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-08 14:53:12 +0530

    Add Typescript declarations for `user`, `image`, `transaction`, `URL query params` and `transformation types`

---
+++ types/index.d.ts
@@ -0,0 +1,142 @@
+/* eslint-disable no-unused-vars */
+
+import { IImage } from "@/lib/database/models/image.model";
+
+// User Params
+declare type CreateUserParams = {
+  clerkId: string;
+  email: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  photo: string;
+};
+
+declare type UpdateUserParams = {
+  firstName: string;
+  lastName: string;
+  username: string;
+  photo: string;
+};
+
+// Image Params
+declare type AddImageParams = {
+  image: {
+    title: string;
+    publicId: string;
+    transformationType: string;
+    width: number;
+    height: number;
+    config: any;
+    secureUrl: string;
+    transformationURL: string;
+    aspectRatio: string | undefined;
+    prompt: string | undefined;
+    color: string | undefined;
+  };
+  userId: string;
+  path: string;
+};
+
+declare type UpdateImageParams = {
+  image: {
+    _id: string;
+    title: string;
+    publicId: string;
+    transformationType: string;
+    width: number;
+    height: number;
+    config: any;
+    secureURL: string;
+    transformationURL: string;
+    aspectRatio: string | undefined;
+    prompt: string | undefined;
+    color: string | undefined;
+  };
+  userId: string;
+  path: string;
+};
+
+declare type Transformations = {
+  restore?: boolean;
+  fillBackground?: boolean;
+  remove?: {
+    prompt: string;
+    removeShadow?: boolean;
+    multiple?: boolean;
+  };
+  removeBackground?: boolean;
+};
+
+
+// Transaction Params
+declare type CheckoutTransactionParams = {
+  plan: string;
+  credits: number;
+  amount: number;
+  buyerId: string;
+};
+
+declare type CreateTransactionParams = {
+  stripeId: string;
+  amount: number;
+  credits: number;
+  plan: string;
+  buyerId: string;
+  createdAt: Date;
+};
+
+declare type TransformationTypeKey =
+  | "restore"
+  | "fill"
+  | "remove"
+  | "recolor"
+  | "removeBackground";
+
+
+  // URL Query Params
+  declare type FormUrlQueryParams = {
+    searchParams: string;
+    key: string;
+    value: string | number | null;
+  };
+
+  declare type UrlQueryParams = {
+    params: string;
+    key: string;
+    value: string | null;
+  };
+
+  declare type RemoveUrlQueryParams = {
+    searchParams: string;
+    keysToRemove: string[];
+  };
+
+  declare type SearchParamProps = {
+    params: {
+      id: string;
+      type: TransformationTypeKey
+    };
+    searchParams: {
+      [key: string]: string | string[] | undefined
+    };
+  };
+
+  declare type TransformationFormProps = {
+    action: "Add" | "Update";
+    userId: string;
+    type: TransformationTypeKey;
+    creditBalance: number;
+    data?: IImage | null;
+    config?: Transformations | null;
+  };
+
+  declare type TransformedImageProps = {
+    image: any;
+    type: string;
+    title: string;
+    transformationConfig: Transformations | null;
+    isTransforming: boolean;
+    hasDownloaded?: boolean;
+    setIsTransforming?: React.Dispatch<React.SetStateAction<boolean>>;
+  };
