6a981da067e3124c1d4a8241419d101e2fe8939d
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-09 03:42:38 +0530

    Implemented AddTransformationTypePage
    Added a new page component for adding `transformations` with `header` & `transformation form integration`

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import Header from '@/components/global/Header';
+import TransformationForm from '@/components/global/TransformationForm';
+import { transformationTypes } from '@/constants';
+import { getUserById } from '@/lib/actions/user.action';
+import { SearchParamProps, TransformationTypeKey } from '@/types';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+import React from 'react';
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+
+  const { userId } = auth();
+  const transformation = transformationTypes[type];
+
+  if(!userId) redirect("/login");
+
+  const user = await getUserById(userId);
+
   return (
-    <div>
-      AddTransformationTypePage
-    </div>
+    <>
+      <Header
+        title={transformation.title}
+        subtitle={transformation.subTitle}
+      />
+
+      <section className='mt-10'>
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
   )
 }
 
