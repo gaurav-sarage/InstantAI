554884d7684becdb87918d2f570edd7206f93742
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-08 14:58:28 +0530

    Imported `FormUrlQueryParams` & `RemoveUrlQueryParams`

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,6 +5,7 @@ import qs from "qs";
 import { twMerge } from "tailwind-merge";
 
 import { aspectRatioOptions } from "@/constants";
+import { FormUrlQueryParams, RemoveUrlQueryParams } from "@/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
