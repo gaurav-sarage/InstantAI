302a3c9402c1ddf773c9113d120529fdb08424e7
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-09 17:27:18 +0530

    Add font, metadata & integrate ClerkProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
-import { ClerkProvider } from "@clerk/nextjs"
+import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
 
-const IBMPlex = IBM_Plex_Sans({
+const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-ibm-plex"
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex'
 });
 
 export const metadata: Metadata = {
-  title: "InstantAI",
-  description: "AI powered Image Generator",
+  title: "Imaginify",
+  description: "AI-powered image generator",
 };
 
 export default function RootLayout({
@@ -23,12 +23,11 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider appearance={{
-      variables: { colorPrimary: "#6C47FF" }
+      variables: { colorPrimary: '#624cf5' }
     }}>
       <html lang="en">
-        <body
-          className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-            {children}
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
         </body>
       </html>
     </ClerkProvider>
