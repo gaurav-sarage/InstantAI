35d58d6f80a17a9b8fb36caacf95501f75dbaa8e
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-09 18:06:29 +0530

    fixed banner-bg file extension

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -70,7 +70,7 @@ module.exports = {
       },
       backgroundImage: {
         "purple-gradient": "url('/assets/images/gradient-bg.svg')",
-        banner: "url('/assets/images/banner-bg.png')",
+        banner: "url('/assets/images/banner-bg.jpg')",
       },
       borderRadius: {
         lg: "var(--radius)",
