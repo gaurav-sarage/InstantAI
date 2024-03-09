d43155d163a3e6bc62ae42434fde6a6a56fe41a2
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-09 17:22:31 +0530

    Refactor: Update Transformation pages
    Refactor transformation pages for `readability`, `consistency` & `authentication checks`

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,30 +1,27 @@
-import Header from '@/components/global/Header';
-import TransformationForm from '@/components/global/TransformationForm';
-import { transformationTypes } from '@/constants';
-import { getUserById } from '@/lib/actions/user.action';
-import { SearchParamProps, TransformationTypeKey } from '@/types';
+import Header from '@/components/shared/Header'
+import TransformationForm from '@/components/shared/TransformationForm';
+import { transformationTypes } from '@/constants'
+import { getUserById } from '@/lib/actions/user.actions';
 import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
-import React from 'react';
 
 const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
-
   const { userId } = auth();
   const transformation = transformationTypes[type];
 
-  if(!userId) redirect("/login");
+  if(!userId) redirect('/sign-in')
 
   const user = await getUserById(userId);
 
   return (
     <>
-      <Header
+      <Header 
         title={transformation.title}
         subtitle={transformation.subTitle}
       />
-
-      <section className='mt-10'>
-        <TransformationForm
+    
+      <section className="mt-10">
+        <TransformationForm 
           action="Add"
           userId={user._id}
           type={transformation.type as TransformationTypeKey}
