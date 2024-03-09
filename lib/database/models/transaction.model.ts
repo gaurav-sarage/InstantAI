df102702e8ec2d2d073f20e4edaa19dfeec27997
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-09 16:59:30 +0530

    Update `image`, `transaction` & `user` models
    Updated `image model` to include `author reference` and additional fields. Added `transaction model` for storing transaction details including `stripeId`, `amt`, `plan` & `buyer reference`. Enhanced `user model` to include more fields such as `firstName`, `lastName`, `planId` & `creditBalance`

--- lib/database/models/transaction.model.ts
+++ lib/database/models/transaction.model.ts
@@ -1,31 +1,31 @@
-import { Schema, models, model } from "mongoose";
+import { Schema, model, models } from "mongoose";
 
-const transactionSchema = new Schema({
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    stripeId: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    amount: {
-        type: Number,
-        required: true,
-    },
-    plan: {
-        type: String,
-    },
-    credits: {
-        type: Number,
-    },
-    buyer: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-    },
+const TransactionSchema = new Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  stripeId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  plan: {
+    type: String,
+  },
+  credits: {
+    type: Number,
+  },
+  buyer: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
-const Transaction = models?.Transaction || model("Transaction", transactionSchema);
+const Transaction = models?.Transaction || model("Transaction", TransactionSchema);
 
 export default Transaction;
