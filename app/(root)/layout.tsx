65e315af6eaa86573bea648097a9d815c0c89f32
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-09 17:26:28 +0530

    Refactor UI components & authentication checks

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,17 +1,20 @@
-import MobileNav from '@/components/global/MobileNav';
-import Sidebar from '@/components/global/Sidebar';
+import MobileNav from '@/components/shared/MobileNav'
+import Sidebar from '@/components/shared/Sidebar'
+import { Toaster } from '@/components/ui/toaster'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className='root'>
+    <main className="root">
       <Sidebar />
       <MobileNav />
 
-      <div className='root-container'>
-        <div className='wrapper'>
-            {children}
+      <div className="root-container">
+        <div className="wrapper">
+          {children}
         </div>
       </div>
+      
+      <Toaster />
     </main>
   )
 }
